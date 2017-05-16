e37e5e1c2dc0a306f20d7ecb2659a59995eb72c4
Bruno Almeida <bruno.fa.almeida@gmail.com>
2017-05-15 22:07:50 -0400

    Implement code to run motors

--- DrivingRobot/DrivingRobot.ino
+++ DrivingRobot/DrivingRobot.ino
@@ -1,10 +1,29 @@
+const int enable1Pin = 10;
+const int enable2Pin = 9;
+const int motor1Pin1 = 3;
+const int motor1Pin2 = 4;
+const int motor2Pin1 = 6;
+const int motor2Pin2 = 7;
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.print("Start");
+
+  pinMode(enable1Pin, OUTPUT);
+  pinMode(enable2Pin, OUTPUT);
+  pinMode(motor1Pin1, OUTPUT);
+  pinMode(motor1Pin2, OUTPUT);
+  pinMode(motor2Pin1, OUTPUT);
+  pinMode(motor2Pin2, OUTPUT);
   
+
+  digitalWrite(enable1Pin, HIGH);
+  digitalWrite(enable2Pin, HIGH);
+  digitalWrite(motor1Pin1, HIGH);
+  digitalWrite(motor1Pin2, LOW);
+  digitalWrite(motor2Pin1, HIGH);
+  digitalWrite(motor2Pin2, LOW);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  
 }
 
